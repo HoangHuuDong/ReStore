dc2a2530ca8fd00367c794925e6482359dbc09a7
TITANCORP\dong.hoangh <dong.hoangh@titancorpvn.com>
2023-07-20 16:24:39 +0700

    Commit on 20/7/23 part 2

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
-import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 
 ReactDOM.render(  
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root')
 );
