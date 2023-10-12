ad1bd9dc1bd278e6f8ab55b8bb81dcf508b7149f
TITANCORP\dong.hoangh <dong.hoangh@titancorpvn.com>
2023-10-12 11:10:48 +0700

    Pagination, Sorting, Filter

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,16 +1,23 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
+import { URLSearchParams } from "url";
+import { PaginatedResponse } from "../models/pagination";
 
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 axios.defaults.withCredentials = true;
 
-const responseBody = (respone: AxiosResponse) => respone.data;
+const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.response.use(async response => {
     await sleep();
+    const pagination = response.headers['pagination']
+    if (pagination) {
+        response.data = new PaginatedResponse(response.data, JSON.parse(pagination))
+        return response
+    }
     return response
 }, (errror: AxiosError) =>{
     const {data, status} = errror.response as AxiosResponse;
@@ -41,17 +48,18 @@ axios.interceptors.response.use(async response => {
 })
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
+    get: (url: string, params?: URLSearchParams) => axios.get(url, {params}).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
     delete: (url: string) => axios.delete(url).then(responseBody),
 }
 
 const Catalog = {
-    list: () => requests.get('products'),
-    details: (id: number) => requests.get(`products/${id}`)
+    list: (params: URLSearchParams) => requests.get('products', params),
+    details: (id: number) => requests.get(`products/${id}`),
+    fetchFilters: () => requests.get('products/filters')
 }
-
+ 
 const TestErrors = {
     get400Error: () => requests.get('buggy/bad-request'),
     get401Error: () => requests.get('buggy/unauthorised'),
